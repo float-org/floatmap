a5b0629834d689591a2530c0d8c8eb459687f0d1
Matteo Banerjee <matteobnrj@users.noreply.github.com>
2014-11-01 13:50:52 -0700

    Revert to png

--- tile_generation/tile_generator.py
+++ tile_generation/tile_generator.py
@@ -178,9 +178,7 @@ def render_tiles(mapfile, tile_dir, minZoom=1,maxZoom=18, name="unknown", num_th
                     str_y = "%s" % ((2**z-1) - y)
                 else:
                     str_y = "%s" % y
-#                tile_uri = '{dir}/{z}/{x}/{y}.png'.format(dir=tile_dir, z=zoom, x=str_x, y=str_y)
-# TEST SVG
-                tile_uri = '{dir}/{z}/{x}/{y}.svg'.format(dir=tile_dir, z=zoom, x=str_x, y=str_y)
+                tile_uri = '{dir}/{z}/{x}/{y}.png'.format(dir=tile_dir, z=zoom, x=str_x, y=str_y)
                 # Submit tile to be rendered into the queue
                 t = (name, tile_uri, x, y, z)
                 try:

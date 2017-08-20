03710b03fe43dae5fec34ac8238a34a21a92875d
Michael Vattuone <mvattuone@Michaels-MacBook-Pro.local>
2017-08-20 13:48:44 -0400

    right, i have to make a route for the static files. bad practice, should fix eventually

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,3 +8,7 @@ urlpatterns = patterns('',
     url(r'^get_queries/$', 'floatmap.views.get_queries', name='get_queries'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += [
+    url(r'^static/(?P<path>.*)$', views.serve)
+]
